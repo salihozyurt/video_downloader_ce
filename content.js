5daa08e8d41cf15abca2fb7af17001b1c59262d5
Salih Özyurt <salih.zyurt@gmail.com>
2021-03-13 22:37:23 +0300

    Extancion is running as logical. CSS file will be edited.

--- content.js
+++ content.js
@@ -1,8 +1,13 @@
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    const videoURL = document.getElementsByTagName("video")[0]["src"]
-    let titleLecture = document.getElementsByClassName("page-title")[0].outerText
-    let tempCourse = document.getElementsByClassName("media-body")[0].outerText
-    let titleCourse = course = tempCourse.substring(0,tempCourse.indexOf(' '))
-    const videoTitle = titleCourse + ' ' + titleLecture
-    sendResponse({url: videoURL, title: videoTitle})
+    const isErrOccur = false
+    try {
+        const videoURL = document.getElementsByTagName("video")[0]["src"]
+        let titleLecture = document.getElementsByClassName("page-title")[0].outerText
+        let tempCourse = document.getElementsByClassName("media-body")[0].outerText
+        let titleCourse = course = tempCourse.substring(0,tempCourse.indexOf(' '))
+        const videoTitle = titleCourse + ' ' + titleLecture
+        sendResponse({url: videoURL, title: videoTitle})
+    } catch (error) {
+        console.log(error)
+    }
 })
--- popup.js
+++ popup.js
@@ -1,27 +1,36 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('catch').addEventListener('click', onclick, false)
+    document.getElementById('catch-video').addEventListener('click', onclick, false)
     function onclick() {
         chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
             chrome.tabs.sendMessage(tabs[0].id, 'Video', getUrl)
         })
-        document.getElementById('catch').disabled = "disabled"
-        document.getElementById('new_tab').style = "visibility: visible;"
+        
     }
 
     let videoUrl = null
     let videoTitle = null
 
-    document.getElementById('new_tab').addEventListener('click', function () {
-        chrome.downloads.download({
-            url: videoUrl,
-            filename: videoTitle
-        })
+    document.getElementById('download-button').addEventListener('click', function () {
+        try {
+            chrome.downloads.download({
+                url: videoUrl,
+                filename: videoTitle
+            })
+        } catch (error) {
+            console.log(error)
+        }
     }, false)
 
     function getUrl(res) {
-        videoUrl = res.url
-        videoTitle = res.title
-        console.log(videoUrl)
+        if(res == undefined) {
+            document.getElementById('intro-p').innerHTML = "Video cannot found!"
+        } else {
+            videoUrl = res.url
+            videoTitle = res.title
+            document.getElementById('video-title').innerHTML = videoTitle
+            document.getElementById('intro').style = "display: none;"
+            document.getElementById('video-div').style = "display: inline-flex;"
+        }
     }
 }, false)
 
